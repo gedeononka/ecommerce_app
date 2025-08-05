a3405df2e0c457d565fee91aa920ab33b360a684
gedeononka <gedeononka4@gmail.com>
2025-08-05 00:32:09 +0000

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(4)->create();
         $this->call([
         AdminSeeder::class,
       ]);
